fcd2c304a993f32acd9c0afc83c20a50b4faf2f2
Beauti1 <91016883+Beauti1@users.noreply.github.com>
2022-05-30 13:28:43 +0530

    largest of two number

---
+++ largest_two.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int no1,no2,max;
+	cout<< "Enter  first number:";
+	cin>> no1;
+	cout<< "Enter  second number:";
+	cin>> no2;
+	if(no1>no2)
+	{
+		cout<< "Largest number is= "<<no1 <<endl;
+	}
+	else
+	{
+		cout<< "Largest number is= "<<no2 <<endl;
+	}
+	return 0;
+}
